1f59fa022756ed28f8c2296e7a3fa71f165342bb
Carlos Astori <tadeu.astori@gmail.com>
2022-12-22 18:11:22 -0500

    added pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppComponent } from './app.component';
 import { FunkoDetailPainelComponent } from './shared/funko-detail-painel/funko-detail-painel.component';
 import { FunkoPainelComponent } from './pages/funko-painel/funko-painel.component';
 import { FormsModule } from '@angular/forms';
+import { MessageAddHomeScreenComponent } from './shared/message-add-home-screen/message-add-home-screen.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,8 +17,19 @@ import { FormsModule } from '@angular/forms';
     FunkoDetailPainelComponent,
     FunkoPainelComponent,
     FilterPipe,
+    MessageAddHomeScreenComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
