0826caaf2ba26f5e55118b047d9ae667f3b60160
Carlos Astori <tadeu.astori@gmail.com>
2022-12-22 17:02:54 -0500

    added version number

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
 })
 export class AppComponent {
   title = 'myfunkocollection';
+
+  appVersion = environment.appVersion;
 }
