26246389f7b3a7c60f959769fde635e1bdc6b197
Carlos Astori <tadeu.astori@gmail.com>
2023-01-06 23:24:15 -0500

    added new funkos

--- src/app/shared/funko-detail-painel/funko-detail-painel.component.ts
+++ src/app/shared/funko-detail-painel/funko-detail-painel.component.ts
@@ -9,5 +9,16 @@ export class FunkoDetailPainelComponent implements OnInit {
   @Input() funkoDetails: any;
   @Input() idFunko: number;
 
+  imageModel = [
+    {
+      name: 'model-box.jpg',
+      order: 1,
+    },
+    {
+      name: 'model-funko.jpeg',
+      order: 2,
+    },
+  ];
+
   ngOnInit() {}
 }
